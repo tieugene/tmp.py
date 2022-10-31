74e5b2ec7c4bad0ba3dd231ab0198abca9c25ae5
TI_Eugene <ti.eugene@gmail.com>
2022-10-31 17:23:34 +0300

    Signal capsulated

--- src/221028.py
+++ src/221028.py
@@ -13,20 +13,23 @@ TODO:
   - [ ] y-scale
   - [ ] y-stretch
   - [ ] y-move
+IDEA: store signal pointer into Signal
 """
 import math
 import random
 # 1. std
 import sys
+from dataclasses import dataclass
+
 # 2. 3rd
 from PyQt5.QtCore import Qt, QObject, QMargins, QRect
-from PyQt5.QtGui import QMouseEvent, QPen
+from PyQt5.QtGui import QMouseEvent, QPen, QColorConstants
 from PyQt5.QtWidgets import QListWidgetItem, QListWidget, QWidget, QMainWindow, QVBoxLayout, QApplication, QSplitter, \
     QPushButton, QHBoxLayout, QTableWidget, QFrame, QHeaderView
 from QCustomPlot2 import QCustomPlot, QCPGraph
-#  x. const
+# x. const
 COLORS = (Qt.black, Qt.red, Qt.green, Qt.blue, Qt.cyan, Qt.magenta, Qt.yellow, Qt.gray)
-BARS = 3  # Signal bars number (each table)
+BARS = 6  # Signal bars number (each table)
 SIN_SAMPLES = 72  # 5°
 SIG_WIDTH = 1.0  # signal width, s
 LINE_CELL_SIZE = 3  # width ow VLine column / height of HLine row
@@ -40,6 +43,15 @@ def y_coords(pnum: int = 1, off: int = 0) -> list[float]:
     :return: Y-coords
     """
     return [round(math.sin((i + off) / SIN_SAMPLES * 2 * math.pi * pnum), 3) for i in range(SIN_SAMPLES + 1)]
+
+
+@dataclass
+class Signal:
+    num: int  # order number
+    name: str
+    pnum: int  # period number
+    off: int  # offset (samples)
+    color: QColorConstants
 
 
 class TopBar(QWidget):
@@ -63,19 +75,13 @@ class BarPlot(QCustomPlot):
 
 
 class SignalSuit(QObject):
-    __name: str  # Label
-    __pnum: int  # Periods number
-    __off: int  # Offset
-    __color: Qt.GlobalColor
+    __signal: Signal
     __label: SignalLabel
     __plot: BarPlot
 
-    def __init__(self, name: str, pnum: int, off: int, color: Qt.GlobalColor):
+    def __init__(self, signal: Signal):
         super().__init__()
-        self.__name = name
-        self.__pnum = pnum
-        self.__off = off
-        self.__color = color
+        self.__signal = signal
         # self.__label = SignalLabel(self.__bar.ctrl.lst)
         # self.__label.setText(name)
 
@@ -192,15 +198,15 @@ class SignalBarTable(QTableWidget):
         self.setCellWidget(row_spl, 2, HLine(bnum, self))
         self.setRowHeight(row_spl, LINE_CELL_SIZE)
 
-    def sig_add(self, bnum: int, name: str, pnum: int, off: int, color: Qt.GlobalColor):
-        sigsuit = SignalSuit(name, pnum, off, color)
+    def sig_add(self, bnum: int, signal: Signal):
+        sigsuit = SignalSuit(signal)
         ctrl = self.cellWidget(bnum * 2, 0)
         lbl = SignalLabel(ctrl.lst)
-        lbl.setText(f"{name}\n{pnum}/{off}")
+        lbl.setText(f"{signal.name}\n{signal.pnum}/{signal.off}")
         plot = self.cellWidget(bnum * 2, 2)
         grf = plot.addGraph()
-        grf.setData(X_COORDS, y_coords(pnum, off), True)
-        grf.setPen(QPen(color))
+        grf.setData(X_COORDS, y_coords(signal.pnum, signal.off), True)
+        grf.setPen(QPen(signal.color))
         # self.sig.add(sigset)
         self.setRowHeight(bnum * 2, 80)
 
@@ -216,11 +222,11 @@ class MainWidget(QWidget):
     lst1: SignalBarTable
     lst2: SignalBarTable
 
-    def __init__(self, parent: QMainWindow):
+    def __init__(self, data: list[Signal], parent: QMainWindow):
         super().__init__(parent)
         self.__mk_widgets()
         self.__mk_layout()
-        self.__set_data()
+        self.__set_data(data)
 
     def __mk_widgets(self):
         self.tb = TopBar(self)
@@ -235,23 +241,27 @@ class MainWidget(QWidget):
         splitter.addWidget(self.lst2)
         self.layout().addWidget(splitter)
 
-    def __set_data(self):
-        for i, lst in enumerate((self.lst1, self.lst2)):
-            for j in range(BARS):
-                lst.bar_insert(j)
-                lst.sig_add(
-                    j,
-                    f"sig{i}/{j}",
-                    random.randint(1, 5),
-                    random.randint(0, SIN_SAMPLES - 1),
-                    COLORS[random.randint(0, len(COLORS) - 1)]
-                )
+    def __set_data(self, data: list[Signal]):
+        def __set_data_one(__tbl: SignalBarTable, __data: list[Signal]):
+            for __j, __sig in enumerate(__data):
+                __tbl.bar_insert(__j)
+                __tbl.sig_add(__j, __sig)
+        n0 = len(data) // 2
+        __set_data_one(self.lst1, data[:n0])
+        __set_data_one(self.lst2, data[n0:])
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setCentralWidget(MainWidget(self))
+        data = [Signal(
+            num=i,
+            name=f"sig{i}",
+            pnum=random.randint(1, 5),
+            off=random.randint(0, SIN_SAMPLES - 1),
+            color=COLORS[random.randint(0, len(COLORS) - 1)]
+        ) for i in range(BARS)]
+        self.setCentralWidget(MainWidget(data, self))
 
 
 def main() -> int:
