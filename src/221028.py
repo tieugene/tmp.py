91207afe6ab85c5a4fcb1e241b042a7c9a864916
TI_Eugene <ti.eugene@gmail.com>
2022-11-01 01:09:38 +0300

    Col0 resize ok

--- src/221028.py
+++ src/221028.py
@@ -3,10 +3,10 @@
 Лента: ribbon, band, lane, strip, set, bar
 TODO:
 - [x] TopBar
-- [ ] Col0 resize sync
-- [ ] HScroller
+- [x] Col0 resize sync
 - [ ] Signal join/move/unjoin (DnD)
 - [ ] Signal hide/unhide
+- [ ] HScroller
 - [ ] Rerange:
   - [ ] x-scale
   - [ ] x-move
@@ -23,10 +23,10 @@ import sys
 from dataclasses import dataclass
 
 # 2. 3rd
-from PyQt5.QtCore import Qt, QObject, QMargins, QRect
+from PyQt5.QtCore import Qt, QObject, QMargins, QRect, pyqtSignal
 from PyQt5.QtGui import QMouseEvent, QPen, QColorConstants, QColor, QFont
 from PyQt5.QtWidgets import QListWidgetItem, QListWidget, QWidget, QMainWindow, QVBoxLayout, QApplication, QSplitter, \
-    QPushButton, QHBoxLayout, QTableWidget, QFrame, QHeaderView, QLabel, QScrollBar, QCommonStyle
+    QPushButton, QHBoxLayout, QTableWidget, QFrame, QHeaderView, QLabel, QScrollBar
 from QCustomPlot2 import QCustomPlot, QCPGraph, QCPAxis
 
 # x. const
@@ -35,7 +35,8 @@ SIN_SAMPLES = 72  # 5°
 SIG_WIDTH = 1.0  # signal width, s
 LINE_CELL_SIZE = 3  # width ow VLine column / height of HLine row
 BAR_HEIGHT = 48
-COL0_INIT_WIDTH = 100
+COL0_WIDTH_INIT = 100
+COL0_WIDTH_MIN = 50
 PEN_NONE = QPen(QColor(255, 255, 255, 0))
 PEN_ZERO = QPen(Qt.black)
 COLORS = (Qt.black, Qt.red, Qt.green, Qt.blue, Qt.cyan, Qt.magenta, Qt.yellow, Qt.gray)
@@ -84,28 +85,33 @@ class TopBar(QWidget):
     __scale: TopPlot
     __stub: QScrollBar
 
-    def __init__(self, parent: 'MainWidget'):
+    def __init__(self, parent: 'OscWindow'):
         super().__init__(parent)
         # widgets
         self.__label = QLabel("ms", self)
         self.__scale = self.TopPlot(self)
         self.__stub = QScrollBar(Qt.Vertical)
-        # print(self.__stub.width())
         # layout
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(self.__label)
         self.layout().addWidget(self.__scale)
         self.layout().addWidget(self.__stub)
         # decorate
-        self.__label.setFixedWidth(COL0_INIT_WIDTH + LINE_CELL_SIZE)
-        self.__label.setFrameShape(QFrame.Box)
-        self.__stub.setStyle(QCommonStyle())
+        # self.__label.setFrameShape(QFrame.Box)
+        # self.__stub.setStyle(QCommonStyle())
         # squeeze
         self.__stub.setFixedHeight(0)
         # self.setContentsMargins(QMargins())
         self.layout().setContentsMargins(QMargins())
         self.layout().setSpacing(0)
         self.__label.setContentsMargins(QMargins())
+        # init sizes
+        self.__label.setFixedWidth(parent.col0_width + LINE_CELL_SIZE)
+        # print(self.parent().metaObject().className())
+        self.parent().signal_resize_col0.connect(self.__slot_resize_col0)
+
+    def __slot_resize_col0(self, x: int):
+        self.__label.setFixedWidth(x + LINE_CELL_SIZE)
 
 
 class SignalLabel(QListWidgetItem):
@@ -187,7 +193,7 @@ class ZoomButtonBox(QWidget):
 
 
 class BarCtrl(QWidget):
-    __bnum: int
+    __bnum: int  # Bar order number in parent table
     lst: SignalLabelList
     zbx: ZoomButtonBox
 
@@ -202,11 +208,11 @@ class BarCtrl(QWidget):
         self.layout().setContentsMargins(QMargins())
 
 
-class HLine(QFrame):
+class HLine(QFrame):  # TODO: incapsulate into SignalBarTable
     __bnum: int
 
     def __init__(self, bnum: int, parent=None):
-        """Parents: QWidget<MainWidget"""
+        """Parents: QWidget<OscWindow"""
         super().__init__(parent)
         self.__bnum = bnum
         self.setGeometry(QRect(0, 0, 0, 0))  # size is not the matter
@@ -218,23 +224,30 @@ class HLine(QFrame):
         (tbl := self.parent().parent()).setRowHeight(self.__bnum * 2, tbl.rowHeight(self.__bnum * 2) + event.y())
 
 
-class VLine(QFrame):
-    def __init__(self, parent=None):
+class VLine(QFrame):  # TODO: incapsulate into SignalBarTable
+    __oscwin: 'OscWindow'
+
+    def __init__(self, oscwin: 'OscWindow', parent=None):
         super().__init__(parent)
+        self.__oscwin = oscwin
         self.setGeometry(QRect(0, 0, 0, 0))  # size is not the matter
         self.setFrameShape(QFrame.VLine)
         self.setCursor(Qt.SplitHCursor)
 
     def mouseMoveEvent(self, event: QMouseEvent):
         """accepted() == True, x() = Δx.
-        TODO: signal to MainWidget => TopBar, Table[], BottomBar
+        :note: parents: QWidget.SignalBarTable.QSplitter.OscWindow
         """
-        (tbl := self.parent().parent()).setColumnWidth(0, tbl.columnWidth(0) + event.x())
+        # (tbl := self.parent().parent()).setColumnWidth(0, tbl.columnWidth(0) + event.x())
+        self.__oscwin.resize_col0(event.x())
 
 
 class SignalBarTable(QTableWidget):
-    def __init__(self, parent: QWidget):
+    __oscwin: 'OscWindow'
+
+    def __init__(self, oscwin: 'OscWindow', parent: QWidget = None):
         super().__init__(parent)
+        self.__oscwin = oscwin
         self.setColumnCount(3)
         self.horizontalHeader().setMinimumSectionSize(1)
         self.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
@@ -242,7 +255,7 @@ class SignalBarTable(QTableWidget):
         self.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
         self.horizontalHeader().setStretchLastSection(True)
         self.horizontalHeader().hide()
-        self.setColumnWidth(0, COL0_INIT_WIDTH)
+        self.setColumnWidth(0, self.__oscwin.col0_width)
         self.setColumnWidth(1, LINE_CELL_SIZE)
         self.verticalHeader().setMinimumSectionSize(1)
         self.verticalHeader().hide()
@@ -250,6 +263,11 @@ class SignalBarTable(QTableWidget):
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)  # not helps
         self.setShowGrid(False)
         self.setDragEnabled(True)
+        # signal/slot
+        self.__oscwin.signal_resize_col0.connect(self.__slot_resize_col0)
+
+    def __slot_resize_col0(self, x: int):
+        self.setColumnWidth(0, x)
 
     def bar_insert(self, bnum: int = -1):
         if bnum < 0 or bnum > (self.rowCount() // 2):
@@ -259,7 +277,7 @@ class SignalBarTable(QTableWidget):
         # signal
         self.insertRow(row_sig)
         self.setCellWidget(row_sig, 0, BarCtrl(bnum, self))
-        self.setCellWidget(row_sig, 1, VLine(self))
+        self.setCellWidget(row_sig, 1, VLine(self.__oscwin, self))
         self.setCellWidget(row_sig, 2, BarPlot(bnum, self))
         self.setRowHeight(row_sig, BAR_HEIGHT)
         # h-splitter
@@ -286,11 +304,13 @@ class SignalBarTable(QTableWidget):
         ...
 
 
-class MainWidget(QWidget):
+class OscWindow(QWidget):
     tb: TopBar
     cb: QWidget
     lst1: SignalBarTable
     lst2: SignalBarTable
+    col0_width = COL0_WIDTH_INIT
+    signal_resize_col0 = pyqtSignal(int)
 
     def __init__(self, data: list[Signal], parent: QMainWindow):
         super().__init__(parent)
@@ -322,6 +342,11 @@ class MainWidget(QWidget):
         __set_data_one(self.lst1, data[:n0])
         __set_data_one(self.lst2, data[n0:])
 
+    def resize_col0(self, dx: int):
+        if self.col0_width + dx > COL0_WIDTH_MIN:
+            self.col0_width += dx
+            self.signal_resize_col0.emit(self.col0_width)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -333,7 +358,7 @@ class MainWindow(QMainWindow):
             off=random.randint(0, SIN_SAMPLES - 1),
             color=COLORS[random.randint(0, len(COLORS) - 1)]
         ) for i in range(BARS)]
-        self.setCentralWidget(MainWidget(data, self))
+        self.setCentralWidget(OscWindow(data, self))
 
 
 def main() -> int:
