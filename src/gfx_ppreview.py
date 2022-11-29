60da12ab9e391e02b340460f7089617fbe90694d
TI_Eugene <ti.eugene@gmail.com>
2022-11-29 20:30:59 +0300

    Toolbar added; "Reset to original" partially works

--- src/gfx_ppreview.py
+++ src/gfx_ppreview.py
@@ -7,11 +7,11 @@ from typing import List
 # 2. 3rd
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QAction, QTableWidgetItem, QDialog, QVBoxLayout,\
-    QGraphicsWidget, QGraphicsLinearLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QAction, QTableWidgetItem, QDialog, QVBoxLayout, \
+    QGraphicsWidget, QGraphicsLinearLayout, QToolBar, QActionGroup, QSizePolicy, QLayout
 # 3. local
 from gfx_ppreview_const import DATA, DataValue, W_PAGE, H_ROW_BASE
-from gfx_ppreview_widgets import GraphView, RowItem, LayoutItem, GraphViewBase, HeaderItem
+from gfx_ppreview_widgets import GraphView, RowItem, LayoutItem, GraphViewBase, HeaderItem, qsize2str
 
 
 class TableItem(QGraphicsWidget):
@@ -37,6 +37,9 @@ class Plot(GraphViewBase):
         self.portrait = False
         self.scene().addItem(TableItem(DATA[:6], self))
 
+    # def sizeHint(self) -> QSize:  # not helps
+    #    return self.scene().itemsBoundingRect().size().toSize()
+
     @property
     def w_full(self) -> int:
         """Current full table width"""
@@ -50,13 +53,60 @@ class Plot(GraphViewBase):
         """
         return round(H_ROW_BASE * 1.5) if self.portrait else H_ROW_BASE
 
+    def reset_size(self):
+        """[Re]set view to original size.
+        .viewport().resize(): not works
+        .setSceneRect(): not works
+        .setFixedSize(): too strict
+        """
+        size = self.scene().itemsBoundingRect().size().toSize()
+        # print(qsize2str(size))
+        self.resize(size)
+        self.parent().adjustSize()  # "The maximum size of a window is 2/3 of the screen's width and height."
+
 
 class ViewWindow(QDialog):
+    __plot: Plot
+    act_size0: QAction  # [re]set size to original
+    act_o: QActionGroup
+    act_o_L: QAction  # landscape
+    act_o_P: QAction  # portrait
+    tb: QToolBar
 
     def __init__(self, parent: QMainWindow):
         super().__init__(parent)
+        self.__plot = Plot(self)
+        self.__mk_actions()
+        self.__mk_toolbar()
         self.setLayout(QVBoxLayout())
-        self.layout().addWidget(Plot(self))
+        self.layout().addWidget(self.tb)
+        self.layout().addWidget(self.__plot)
+        # experiments
+        self.layout().setSpacing(0)
+        # self.layout().setSizeConstraint(QLayout.SizeConstraint.SetNoConstraint)
+        # self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+
+    def __mk_actions(self):
+        self.act_size0 = QAction(QIcon.fromTheme("zoom-original"), "Original size", self, shortcut="Ctrl+0",
+                                 triggered=self.__plot.reset_size)
+        self.act_o_L = QAction(QIcon.fromTheme("object-flip-horizontal"), "Landscape", self, shortcut="Ctrl+L",
+                               checkable=True)
+        self.act_o_P = QAction(QIcon.fromTheme("object-flip-vertical"), "Portrait", self, shortcut="Ctrl+P",
+                               checkable=True)
+        self.act_o = QActionGroup(self)
+        self.act_o.addAction(self.act_o_L).setChecked(True)
+        self.act_o.addAction(self.act_o_P)
+        self.act_o.triggered.connect(self.__do_o)
+
+    def __mk_toolbar(self):
+        self.tb = QToolBar(self)
+        self.tb.addAction(self.act_size0)
+        self.tb.addAction(self.act_o_L)
+        self.tb.addAction(self.act_o_P)
+
+    def __do_o(self, a: QAction):
+        """Switch page orientation"""
+        ...
 
 
 class MainWidget(QTableWidget):
